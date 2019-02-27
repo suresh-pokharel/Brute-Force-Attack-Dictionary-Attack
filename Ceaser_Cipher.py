729d7989ee696527bd5210d21e48d5ed1e2ebc0f
Suresh Pokharel <suresh.wrc@gmail.com>
2019-02-27 17:13:29 +0545

    encrypt

--- Ceaser_Cipher.py
+++ Ceaser_Cipher.py
@@ -1,4 +1,15 @@
 
 class CeaserCipher(object):
     """Ceaser Cipher Implementation"""
+    
+    
+    def encrypt(self,text, k):
+        cipher = []
+        for character in text:
+            # convert character to ascii and add k
+            temp = ord(character) + k
 
+            # convert ascii to character
+            cipher.append(chr(temp))
+
+        return cipher
